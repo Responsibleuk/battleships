7d552681e5d393cee44cb72bb05fadc3968886f7
Responsibleuk <james@we-are-responsible.com>
2022-02-14 16:43:28 +0000

    Add differnt way to build grids

--- run.py
+++ run.py
@@ -1,42 +1,33 @@
 from random import randint
 
-# Game board - 10 x 10
+# Legend
+# "@" for placing ship
+# " " for available space
+# "X" for hit battleship
+# "-" for missed shot
+# Code taken from Knowledge Mavens video, 
+HIDDEN_BOARD = [[" "] * 10 for x in range(8)]
 
-def create_board(board)
-    for i in range(10):
-        board.append ([" x "]*10)
-    return board
-
-# Starting game
-print ("Are you ready to defend our waters?")
-print ("4 ships need to be sunk")
-print ("You have 50 torpedoes, choose your shots carefully")
-print_board(board)
+GUESS_BOARD = [[" "] * 10 for x in range(8)]
 
 
-# Create ships
-def create_ships(board):
-    ship_number = 0
+
+
+letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4,
+                      "F": 5, "G": 6, "H": 7, "I": 8, "J": 9}
+
+
+numbers_to_letters = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E",
+                      5: "F", 6: "G", 7: "H", 8: "I" 9: "J"}
+
+def print_board(board):
     
-
-# Create position
-def random_number(board):
-    return randint(0, len(board)-1)
-
-def get_ship_position():
-    ship_number = 0
-    while ship_number < 5:
-        ship_row = random_number(board)
-        ship_column = random_number(board)
-        
-
-# Take first shot
-print ("Take your first shot")
-
-# Hit or miss
-def count_hit_ships(board):
-
-
-# Win or loose
+   
+    print("  A B C D E F G H I J")
+    print("  -----------------")
+    row_number = 1
+    for row in board:
+        print(row_number, "|".join(row))
+        row_number += 1
 
 
